e7a1d7af200cda2b039e615f29ecfe4224db5f37
Evan Kalaw <e.c.kalaw@gmail.com>
2017-07-26 12:33:53 -0700

    Slider functionality?

--- calculations.js
+++ calculations.js
@@ -9,6 +9,7 @@ var light = false;
 var moderate = false;
 var heavy = false;
 var extreme = false;
+var deficit = 0;
 var genderButtons = document.querySelectorAll(".genderButton");
 var ageInput = document.querySelector("#age");
 var ft = document.querySelector("#height_major");
@@ -16,8 +17,10 @@ var inches = document.querySelector("#height_minor");
 var weight = document.querySelector("#weight");
 var bodyFat = document.querySelector("#bodyfat");
 var activityLevel = document.querySelector("#activityLevel");
+var deficitSlider = document.getElementById("deficitRange");
+var deficitPlaceHolder = document.getElementById("deficitPHold");
 
-//Add functionality to radio buttons
+//Add functionality to all inputs
 genderButtons[0].addEventListener("click", function() {
 	isMale = true;
 });
@@ -50,3 +53,15 @@ activityLevel.addEventListener("change", function() {
 	console.log(this.value);
 });
 
+deficitPlaceHolder.textContent = deficit + "% (maintenance)";
+
+deficitSlider.addEventListener("input", function() {
+	deficit = Number(deficitSlider.value);
+	if (deficit < 0) {
+			deficitPlaceHolder.textContent = deficit + "% deficit";
+	} else if (deficit > 0) {
+		deficitPlaceHolder.textContent = deficit + "% surplus";
+	} else {
+		deficitPlaceHolder.textContent = deficit + "% (maintenance)";
+	}
+});
